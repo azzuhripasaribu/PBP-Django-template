ffcf32d1b84e9c9bacdda862cc081b198d07a317
Azzuhri Nauli Pasaribu <azzuhri.nauli@ui.ac.id>
2022-09-15 10:19:43 +0700

    Lab02

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist, show_xml,show_json_id
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/<int:id>', show_json_id, name='show_json_id')
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,7 @@
+from ctypes.wintypes import HRSRC
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 from wishlist.models import ItemWishlist
 
 def show_wishlist(request):
@@ -8,3 +11,12 @@ def show_wishlist(request):
         'name': 'Cinoy'
     }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data),
+     content_type="application/xml")
+
+def show_json_id(request,id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data))
