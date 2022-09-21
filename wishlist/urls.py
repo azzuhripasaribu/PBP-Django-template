c60edb94e44d7b47682265275a3252b3ed4fa1a2
Azzuhri Nauli Pasaribu <azzuhri2003@gmail.com>
2022-09-21 23:17:28 +0700

    Lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from wishlist.views import show_wishlist, show_xml,show_json_id
+from wishlist.views import register, login_user, logout_user
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
     path('xml/', show_xml, name='show_xml'),
-    path('json/<int:id>', show_json_id, name='show_json_id')
+    path('json/<int:id>', show_json_id, name='show_json_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,14 +1,21 @@
-from ctypes.wintypes import HRSRC
-from django.shortcuts import render
-from django.http import HttpResponse
+import datetime
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.core import serializers
 from wishlist.models import ItemWishlist
 
+@login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
     data_wishlist_item = ItemWishlist.objects.all()
     context = {
         'list_item': data_wishlist_item,
-        'name': 'Cinoy'
+        'name': 'Cinoy',
+        'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
 
@@ -20,3 +27,38 @@ def show_xml(request):
 def show_json_id(request,id):
     data = ItemWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data))
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account successfully created!')
+            return redirect('wishlist:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user) # login first
+            response = HttpResponseRedirect(reverse("wishlist:show_wishlist")) # create response
+            response.set_cookie('last_login', str(datetime.datetime.now())) # create last_login cookie and add it to response
+            return response
+        else:
+            messages.info(request, 'Wrong Username or Password!')
+    context = {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('wishlist:login'))
+    response.delete_cookie('last_login')
+    return response
+
